ada1e8999248059e79146cebc5abc98a5a4ad724
Administrator <Administrator@COM1324.wacoal.co.th>
2017-11-29 08:00:08 +0700

    ทดสอบ 28/11/2017

--- src/main/java/com/wacoal/dockeeper/MainController.java
+++ src/main/java/com/wacoal/dockeeper/MainController.java
@@ -5,41 +5,80 @@
  */
 package com.wacoal.dockeeper;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.wacoal.dockeeper.wsdl.EmpClass;
+import com.wacoal.dockeeper.wsdl.GetEmpByFilterResponse;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.List;
+import java.util.Map;
+import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
  *
- * @author Administrator
+ * @author sommaik
  */
 @RestController
 @RequestMapping("/main")
 public class MainController {
 
-//    @RequestMapping(method = RequestMethod.POST, value = "/") // URL folder
-//    public String index() {
-//        return "Hello World";
-//    }
+    @Autowired
+    EmpClient empClient;
 
-    @RequestMapping(method = RequestMethod.GET, value = "") // URL folder
-    public ModelAndView index() {
-        return new ModelAndView("main"); // file main.html
+    @Autowired
+    DataSource datasource;
+
+    @RequestMapping(method = RequestMethod.GET, value = "")
+    public ModelAndView index(Model model) {
+        GetEmpByFilterResponse resp = empClient.getEmpByFilter();
+        List<EmpClass> emps = resp.getGetEmpByFilterResult().getEmpClass();
+        EmpClass emp = emps.get(0);
+        model.addAttribute("emp", emp);
+        return new ModelAndView("main");
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "/{name}")
-    public String getName(@PathVariable String name) {
-        return "Hello " + name;
+    public String getName(
+            @PathVariable String name
+    ) throws Exception {
+        StringBuilder restUrl = new StringBuilder("http://twcapi.wacoalsampan.com/api/emp/search/v1?empname=");
+        restUrl.append(name)
+                .append("&api_token=BI8YXJFZmvYGhhx9MjjoLcC8mLMza85oUN9uJtH7uMUEORlqhTNBQ6fMEZXn");
+
+        RestTemplate rest = new RestTemplate();
+        String resp = rest.getForObject(restUrl.toString(), String.class);
+        ObjectMapper mapper = new ObjectMapper();
+        Map<String, String>[] map = mapper.readValue(resp, new TypeReference<Map<String, String>[]>() {
+        });
+        System.out.println(map[0].get("nameth"));
+        return resp;
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "/some")
     public String getSomeParam(
             @RequestParam(value = "name", required = false, defaultValue = "World") String name
-    ) {
+    ) throws Exception {
+        Connection con = this.datasource.getConnection();
+        Statement stmt = con.createStatement();
+        ResultSet res = stmt.executeQuery("select * from tb_attach");
+        while (res.next()) {
+            System.out.println(res.getString("attach_file"));
+        }
+        res.close();
+        stmt.close();
+        con.close();
         return "Some " + name;
     }
 
@@ -49,4 +88,5 @@ public class MainController {
     ) {
         return "Some " + name;
     }
+
 }
--- src/main/java/com/wacoal/dockeeper/WebSecurityConfig.java
+++ src/main/java/com/wacoal/dockeeper/WebSecurityConfig.java
@@ -1,58 +1,43 @@
 package com.wacoal.dockeeper;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.encoding.Md5PasswordEncoder;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
-@Configuration
-@EnableWebSecurity
+//@Configuration
+//@EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/", "/home").permitAll()
+                .antMatchers("/", "/home", "/ws/*").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
                 .loginPage("/login")
+                .defaultSuccessUrl("/main")
                 .permitAll()
                 .and()
                 .logout()
                 .permitAll();
     }
-
-//    @Autowired 
+    
+//    @Autowired
 //    public void configureGlobal(AuthenticationManagerBuilder auth) 
 //            throws Exception {
 //        auth
 //            .inMemoryAuthentication()
-//                .withUser("user").password("password").roles("USER"); // Fix ID: user PASS: password for test
+//                .withUser("user").password("pwd").roles("USER");
 //    }
-//    @Override
-//    public void configure(AuthenticationManagerBuilder auth) throws Exception {
-//        auth
-//                .ldapAuthentication()
-//                .userDnPatterns("uid={0},ou=user")
-//                .groupSearchBase("ou=groups")
-//                .contextSource()
-//                .url("ldap://10.11.9.135:389/dc=wacoal,dc=co,dc=th")
-//                .and()
-//                .passwordCompare()
-//                .passwordEncoder(new LdapShaPasswordEncoder())
-//                .passwordEncoder(new Md5PasswordEncoder())
-//                .passwordAttribute("userPassword");
-//    }
+
     @Override
     public void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth
                 .ldapAuthentication()
-                .userDnPatterns("uid={0},ou=user")
-                .contextSource() 
+                .userDnPatterns("uid={0}, ou=user")
+                .contextSource()
                 .url("ldap://10.11.9.135:389/dc=wacoal,dc=co,dc=th");
     }
 }
